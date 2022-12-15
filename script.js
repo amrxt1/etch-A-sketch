48a6feafd4beaaef54bcab3be154cd4fa97c4c26
amrxt <amrxt11@outlook.com>
2022-12-15 19:39:44 +0530

    Fix the logic behind the working of magic, black and reset buttons

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //code that creates and appends divs(grids) to the container
 const container = document.querySelector('.container');
-let x = 0;
+let x = 16;
 
 function createGrid(x){
-
+    const divs = document.querySelectorAll('.grid');
+    divs.forEach(div => {
+        div.remove();
+    })
     const sideLength = 500.0/x;
     const numberOfGrids = x*x;
     for(let i = 1; i<=numberOfGrids; i++){
@@ -14,8 +17,8 @@ function createGrid(x){
         container.appendChild(div);
     }
 }
-createGrid(16);
-
+createGrid(x);
+enableBlack();
 //button code below to set resolution
 const btn = document.querySelector('.btn');
 btn.addEventListener('click',promptGrid);
@@ -26,33 +29,38 @@ function promptGrid(e){
         alert("This number cannot be larger than 100.");
         return;
     }
+    createGrid(x);
+    enableBlack();
+}
+
+//code for reset button
+const reset = document.querySelector('.reset');
+reset.addEventListener('click',l);
+function l(){
     const divs = document.querySelectorAll('.grid');
     divs.forEach(div => {
-        div.remove();
+        div.style.backgroundColor = 'beige';
     })
-    createGrid(x);
-    enableChangingColors('black');
 }
 
-//below is the code that changes colors
-enableChangingColors('black');
+//code for black button
+const black = document.querySelector('.black');
+black.addEventListener('click',enableBlack);
 
-function enableChangingColors(string){
+function enableBlack(){
     const divs = document.querySelectorAll('.grid');
-    if(string === 'black'){    
-        divs.forEach(div => div.addEventListener('mouseenter',changeColorBlack));
-    }
-    else{
-        divs.forEach(div => div.addEventListener('mouseenter',changeColor));
-    }
-}
-function changeColor(e){
-    this.style.backgroundColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
-}
-function changeColorBlack(e){
-    this.style.backgroundColor = "#000000";
+    divs.forEach(div => div.addEventListener('mouseenter',()=>{
+        div.style.backgroundColor = 'black';
+    }));
 }
 
-//code for magic colors button
-const mgBtn = document.querySelector('.magic');
-mgBtn.addEventListener('click',enableChangingColors);
+//code for magic button
+const magic = document.querySelector('.magic');
+magic.addEventListener('click',enableMagic);
+
+function enableMagic(){
+    const divs = document.querySelectorAll('.grid');
+    divs.forEach(div => div.addEventListener('mouseenter',()=>{
+        div.style.backgroundColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
+    }));
+}
