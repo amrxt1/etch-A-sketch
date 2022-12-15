c955baadf18636fd3bb7fbb667c5c056654e282a
amrxt <amrxt11@outlook.com>
2022-12-15 17:01:47 +0530

    Make the change resolution button work.

--- script.js
+++ script.js
@@ -15,11 +15,33 @@ function createGrid(x){
     }
 }
 createGrid(16);
-//below is the code that changes colors
-const divs = document.querySelectorAll('.grid');
-divs.forEach(div => div.addEventListener('mouseenter',changeColor));
-console.log(divs);
 
+//button code below to set resolution
+const btn = document.querySelector('.btn');
+btn.addEventListener('click',promptGrid);
+
+function promptGrid(e){
+    let x = prompt('Enter the grid length you want.');
+    if(x>100){
+        alert("This number cannot be larger than 100.");
+        return;
+    }
+    const divs = document.querySelectorAll('.grid');
+    divs.forEach(div => {
+        div.remove();
+    })
+    createGrid(x);
+    enableChangingColors();
+}
+
+//below is the code that changes colors
+enableChangingColors();
+
+function enableChangingColors(){
+    const divs = document.querySelectorAll('.grid');
+    divs.forEach(div => div.addEventListener('mouseenter',changeColor));
+    console.log(divs);
+}
 function changeColor(e){
     this.style.backgroundColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
 }
