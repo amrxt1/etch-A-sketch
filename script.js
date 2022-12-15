bb38a0f3ee43edf7bad5d941f4b8dd4d1d197f40
amrxt <amrxt11@outlook.com>
2022-12-15 15:17:23 +0530

    Make the grids change to random color on mouseenter

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ for(let i = 0; i<=255; i++){
     div.style.padding = 0;
     //div.style.border = '0.5px solid black';
     div.classList.add('grid');
-    div.style.backgroundColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
     container.appendChild(div);
+}
+
+const divs = document.querySelectorAll('.grid');
+divs.forEach(div => div.addEventListener('mouseenter',changeColor));
+console.log(divs);
+
+function changeColor(e){
+    this.style.backgroundColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
 }
